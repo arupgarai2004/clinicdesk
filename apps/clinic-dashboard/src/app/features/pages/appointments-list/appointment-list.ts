bcd8a78f99e78e8d58ab96f065f95e0af198c2e0
Arup Garai <arupgarai2004@gmail.com>
2026-04-10 16:51:27 +0200

    add header and fotter and scrolling

--- apps/clinic-dashboard/src/app/features/pages/appointments-list/appointment-list.ts
+++ apps/clinic-dashboard/src/app/features/pages/appointments-list/appointment-list.ts
@@ -1,11 +1,12 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { AppointmentStore } from '@org/data-access';
 import { computed } from '@angular/core';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 
 @Component({
   selector: 'app-appointment-list',
   standalone: true,
-  imports: [],
+  imports: [ScrollingModule],
   templateUrl: './appointment-list.html',
   styleUrl: './appointment-list.scss',
 })
@@ -14,6 +15,7 @@ export class AppointmentList implements OnInit {
   appointmentStore = inject(AppointmentStore);
 
   appointmentList = computed(() => this.appointmentStore.appointments());
+  trackById = (_: number, item: any) => item.id;
   ngOnInit() {
     this.appointmentStore.loadAppointments();
   }
---
+++ libs/ui-layout/src/index.ts
@@ -0,0 +1,3 @@
+export * from './lib/ui-layout/ui-layout';
+export * from './lib/ui-layout/header/header';
+export * from './lib/ui-layout/footer/footer';
