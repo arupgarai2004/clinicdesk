46d8d9e20295f3b9f0a8d7a55d0ca24af84d1894
Arup Garai <arupgarai2004@gmail.com>
2026-04-14 14:40:59 +0200

    css issue fixes

--- libs/ui-layout/src/lib/ui-layout/header/header.ts
+++ libs/ui-layout/src/lib/ui-layout/header/header.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'lib-header',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header {}
+export class Header { }
